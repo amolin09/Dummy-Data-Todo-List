6d0460c095110d9dbe04284c8d49fbc289e2dc9a
amolin09 <alex.molina5829@gmail.com>
2023-02-27 20:21:10 -0600

    populted todo list

---
+++ scripts.js
@@ -0,0 +1,37 @@
+let arrayOfTodos = [
+  {
+    userId: 14,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  },
+  {
+    userId: 20,
+    id: 2,
+    title: "delectus aut autem",
+    completed: false
+  }
+]
+
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then((response) => response.json())
+  .then((json) => arrayOfTodos =json)
+  console.log('Fetched Todos')
+}
+
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
+
+const populateTodos = () =>{
+ const todoList = document.getElementById("todo-list")
+
+ for(i = 0; i <= arrayOfTodos.length; i++){
+  const todoItem = arrayOfTodos[i]
+  const todoLi = document.createElement('LI')
+  const todoItemTitle = document.createTextNode(todoItem.title)
+  todoLi.appendChild(todoItemTitle)
+  todoList.appendChild(todoLi)
+ }
+}
